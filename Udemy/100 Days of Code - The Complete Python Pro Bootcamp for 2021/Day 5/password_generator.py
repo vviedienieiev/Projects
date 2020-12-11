862cb4df287fccaf179c9530a099e458f41ce5b6
Volodymyr Viedienieiev <45690998+vviedienieiev@users.noreply.github.com>
2020-12-11 13:55:29 +0200

    Create password_generator.py

---
+++ Udemy/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 5/password_generator.py
@@ -0,0 +1,23 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+total_available_chars = [] + ["L"]*nr_letters + ["S"]*nr_symbols+["N"]*nr_numbers
+for char in range(len(total_available_chars)):
+  symb_left = len(total_available_chars)
+  choice = total_available_chars[random.randint(0,symb_left-1)]
+  if choice == "L":
+    password += letters[random.randint(0,len(letters)-1)]
+  elif choice == "S":
+    password += symbols[random.randint(0,len(symbols)-1)]
+  else:
+    password += numbers[random.randint(0,len(numbers)-1)]
+  total_available_chars.remove(choice)
+print(f"Password is {password}")
