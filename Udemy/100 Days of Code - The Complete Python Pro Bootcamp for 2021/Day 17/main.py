a0b882c252cd89302a0b1b2a27d79a59b349723d
Volodymyr Viedienieiev <45690998+vviedienieiev@users.noreply.github.com>
2020-12-20 15:06:50 +0200

    Create main.py

---
+++ Udemy/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 17/main.py
@@ -0,0 +1,15 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for q in question_data:
+  question_bank.append(Question(q["text"], q["answer"]))
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+  quiz.next_question()
+
+print("You've complited the quiz!")
+print(f"Your final score is {quiz.score}/{quiz.question_number}!")
