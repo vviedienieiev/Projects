7650f8762c43b2d5c1bc9f8e93a38942879d4350
Volodymyr Viedienieiev <volodymyr.vedeneev@gmail.com>
2020-12-25 12:39:47 +0200

    add day 20-21 Snake main

--- Udemy/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 20-21/snake.py
+++ Udemy/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 20-21/snake.py
@@ -1,11 +1,12 @@
 from turtle import Turtle
 
-STARTING_POSITION = [(0,0), (-20,0), (-40,0)]
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+
 
 class Snake:
 
@@ -16,11 +17,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITION:
-            snake_segment = Turtle(shape="square")
-            snake_segment.color("white")
-            snake_segment.penup()
-            snake_segment.goto(position)
-            self.segments.append(snake_segment)
+            self.add_segment(position)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def add_segment(self, position):
+        snake_segment = Turtle(shape="square")
+        snake_segment.color("white")
+        snake_segment.penup()
+        snake_segment.goto(position)
+        self.segments.append(snake_segment)
 
     def move(self):
         for segment_num in range(len(self.segments) - 1, 0, -1):
@@ -44,4 +51,3 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
-
