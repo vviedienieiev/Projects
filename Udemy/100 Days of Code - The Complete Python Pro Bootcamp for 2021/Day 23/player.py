0eba1c5835a788962f42632f22b42683783a4bce
Volodymyr Viedienieiev <volodymyr.vedeneev@gmail.com>
2020-12-26 13:40:34 +0200

    add day 23 turtle race

---
+++ Udemy/100 Days of Code - The Complete Python Pro Bootcamp for 2021/Day 23/player.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.setheading(90)
+        self.penup()
+        self.go_to_start()
+
+
+    def move(self):
+        new_x = self.xcor()
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(new_x, new_y)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
